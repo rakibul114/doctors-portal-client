afe547a877de41d336210665f940796bfc8b9609
rakibul114 <rakibulislam114@gmail.com>
2022-05-10 15:41:24 +0600

    treatment banner added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Banner from './Banner';
+import BannerBottom from './BannerBottom';
 import Info from './Info';
+import Services from './Services';
 
 const Home = () => {
     return (
-        <div>
-            <Banner></Banner>
-            <Info></Info>
-        </div>
+      <div className="container px-12 mx-auto">
+        <Banner></Banner>
+        <Info></Info>
+            <Services></Services>
+            <BannerBottom></BannerBottom>
+      </div>
     );
 };
 
