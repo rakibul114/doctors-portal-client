6e280973a8e08f4abdf79e24b17e84584fee6ecf
rakibul114 <rakibulislam114@gmail.com>
2022-05-10 22:37:22 +0600

    footer section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,19 +2,21 @@ import React from 'react';
 import Appointment from './Appointment';
 import Banner from './Banner';
 import BannerBottom from './BannerBottom';
+import Footer from './Footer';
 import Info from './Info';
 import Services from './Services';
 import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
-      <div className="container px-12 mx-auto">
+      <div className="mx-auto">
         <Banner></Banner>
         <Info></Info>
-            <Services></Services>
+        <Services></Services>
         <BannerBottom></BannerBottom>
         <Appointment></Appointment>
         <Testimonials></Testimonials>
+        <Footer></Footer>
       </div>
     );
 };
