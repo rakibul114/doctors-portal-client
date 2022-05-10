8634d72ce278182a6102cbd84553d9ad9c8aa094
rakibul114 <rakibulislam114@gmail.com>
2022-05-10 22:23:56 +0600

    appointment banner added

---
+++ src/Pages/Home/Review.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Review = ({review}) => {
+    return (
+      <div className="card w-96 bg-base-100 shadow-xl mx-auto">
+        <div className="card-body">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, quaerat architecto reiciendis tempora ex magnam ratione
+            modi a natus, id, corporis odit. Culpa, magnam provident in tempora
+            odit error! Voluptas?
+          </p>
+        </div>
+        <div className='flex items-center p-5 gap-5'>
+          <div class="avatar">
+            <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img src={review.img} alt='' />
+            </div>
+                </div>
+                <div>
+                    <h4 className='text-xl'>{review.name}</h4>
+                    <p>{review.location}</p>
+                </div>
+        </div>
+      </div>
+    );
+};
+
+export default Review;
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg';
+import people1 from '../../assets/images/people1.png';
+import people2 from '../../assets/images/people2.png';
+import people3 from '../../assets/images/people3.png';
+import Review from './Review';
 
 const Testimonials = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: '',
+            location: 'Alaska',
+            img: people1
+        },
+        {
+            _id: 2,
+            name: 'Nina Bewes',
+            review: '',
+            location: 'Arizona',
+            img: people2
+        },
+        {
+            _id: 3,
+            name: 'Will Naguan',
+            review: '',
+            location: 'California',
+            img: people3
+        }
+    ];
     return (
       <section className='my-20'>
         <div className='flex justify-between'>
@@ -13,7 +40,11 @@ const Testimonials = () => {
                 <img className='w-24 lg:w-48' src={quote} alt="" />
                 </div>
         </div>
-        <div></div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-0'>
+                {
+                    reviews.map(review => <Review key={review._id} review={review}></Review>)
+            }
+            </div>
       </section>
     );
 };
