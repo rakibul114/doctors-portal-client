dcf3753508cdf612e4d0b7022571f3e95d2254a9
rakibul114 <rakibulislam114@gmail.com>
2022-05-11 10:25:50 +0600

    added a modal for booking appointment

--- src/Pages/Appointment/AppointService.js
+++ src/Pages/Appointment/AppointService.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
-const AppointService = ({ singleService }) => {
-  const {name, slots} = singleService;
+const AppointService = ({ singleService, setTreatment }) => {
+  const { name, slots } = singleService;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
       <div className="card-body text-center">
         <h2 className="card-title mx-auto text-secondary">{name}</h2>
         <p>
-          {
-            slots.length > 0 
-              ? <span>{slots[0]}</span>
-              : <span className='text-red-500'>Try another date</span>
-        }
+          {slots.length > 0 ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-red-500">Try another date</span>
+          )}
         </p>
-        <p>{slots.length } {slots.length > 1 ? 'spaces' : 'space'} available</p>
-        <div className="card-actions justify-center">
-          <button disabled={slots.length ===0} className="btn btn-secondary text-white">Book Appointment</button>
+        <p>
+          {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <div className="card-actions justify-center">          
+          <label
+            onClick={() => setTreatment(singleService)}
+            disabled={slots.length === 0}
+            for="booking-modal"
+            class="btn btn-secondary text-white"
+          >
+            Book Appointment
+          </label>
         </div>
       </div>
     </div>
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,10 +1,12 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import AppointService from './AppointService';
+import BookingModal from './BookingModal';
 
 
 const AvailableAppointment = ({ date }) => {
-    const [services, setServices] = useState([]);
+  const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
         fetch('services.json')
@@ -16,14 +18,16 @@ const AvailableAppointment = ({ date }) => {
     return (
       <div className="text-xl text-primary text-center">
         Available Appointments on {format(date, "PP")}
-        <div className='container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 lg:justify-center'>
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 lg:justify-center">
           {services.map((singleService) => (
             <AppointService
               key={singleService._id}
               singleService={singleService}
+              setTreatment={setTreatment}
             ></AppointService>
           ))}
         </div>
+        {treatment && <BookingModal treatment={treatment}></BookingModal>}
       </div>
     );
 };
